1ff424ed67104260f1e3e23b69778af270e6218d
max <maxjunhuixu@gmail.com>
2020-01-27 22:37:45 +0800

    Updates

---
+++ precache-manifest.dc407a0bfab593671151d6bbe179be5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263825d6e4b3e3d2e0d3e8c9fc2af156",
+    "url": "/forum/index.html"
+  },
+  {
+    "revision": "9133ca49178459da81dc",
+    "url": "/forum/static/css/main.3ebcc155.chunk.css"
+  },
+  {
+    "revision": "00e9f2029cd43f7db492",
+    "url": "/forum/static/js/2.4582e46a.chunk.js"
+  },
+  {
+    "revision": "25567acaed72bdc8fd3c8bc4daa6e367",
+    "url": "/forum/static/js/2.4582e46a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9133ca49178459da81dc",
+    "url": "/forum/static/js/main.278f9750.chunk.js"
+  },
+  {
+    "revision": "46d90e1136aa57a24e63",
+    "url": "/forum/static/js/runtime-main.cf5beb19.js"
+  }
+]);
